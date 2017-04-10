686036954d74ccc78a48c5194a088e8726502411
agiddings <amgiddings@gmail.com>
2017-04-09 23:21:27 -0400

    Added step state machine

--- main.py
+++ main.py
@@ -12,12 +12,10 @@ instructionnum = 0
 
 @ask.intent("HelloIntent")
 def hello():
-	instructionnum++
     return statement("Hello friendo!")
 
 @ask.launch
 def start_skill():
-	instructionnum++
     return statement("What do you want to fix today?")
 
 @ask.intent("AMAZON.YesIntent")
@@ -27,29 +25,42 @@ def yes_intent():
 
 @ask.intent("AMAZON.NoIntent")
 def no_intent():
-	instructionnum++
     return statement("Goodbye")
 
 @ask.intent("AMAZON.RepeatIntent")
 def repeat_intent():
-	instructionnum++
-	return statement(step1)
-
+	if instructionnum == 1:
+		return statement(step1)
+	else if instructionnum == 2:
+		return statement(step2)
+	else if instructionnum == 3:
+		return statement(step3))
+	return ("You are not currently using an instruction guide.")
+		
 @ask.intent("AMAZON.NextIntent")
 def next_intent():
 	instructionnum++
-	if instructionnum == 3:
+	if instructionnum == 1:
+		return statement(step1)
+	else if instructionnum == 2:
 		return statement(step2)
-	if instructionnum == 5:
+	else if instructionnum == 3:
 		return statement(step3)
 	return statement("You have completed the guide. Would you like to start a new project?")
 	
 @ask.intent("AMAZON.PreviousIntent")
 def previous_intent():
-	instructionnum++
-    return statement(step2)
+	instructionnum--
+	if instructionnum == 0:
+		return statement("There are no previous instructions.")
+	else if instructionnum == 1:
+	return statement(step1)
+	else if instructionnum == 2:
+		return statement(step2)
+	else if instructionnum == 3:
+		return statement(step3)
+    return statement("You are not currently using an instruction guide.")
 
 @ask.intent("ItemIntent")
 def item_intent():
-	instructionnum++
     return statement("I sent a list of possible guides through the alexa app. Please choose one or try searching again.")
