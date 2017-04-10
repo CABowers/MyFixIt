38a769accaa6f2575ebbe7bfd5686618e02efc40
agiddings <amgiddings@gmail.com>
2017-04-09 23:12:24 -0400

    Added return values for intent functions

--- main.py
+++ main.py
@@ -4,34 +4,52 @@ from flask_ask import Ask, statement
 app = Flask(__name__)
 ask = Ask(app, "/")
 
+step1 = "Step 1: Put the new staples in! Close the stapler with the new staples in it."
+step2 = "Step 2 : Put the new staples in! Close the stapler with the new staples in it."
+step3 = "Step 3 : Start stapling stuff!"
+
+instructionnum = 0
+
 @ask.intent("HelloIntent")
 def hello():
+	instructionnum++
     return statement("Hello friendo!")
 
 @ask.launch
 def start_skill():
+	instructionnum++
     return statement("What do you want to fix today?")
 
 @ask.intent("AMAZON.YesIntent")
 def yes_intent():
-    return statement("YES!")
+	instructionnum++
+    return statement("You have selected Stapler Maintenance. This guide requires a stapler and extra staples." + step1)
 
 @ask.intent("AMAZON.NoIntent")
 def no_intent():
-    return statement("NO!")
+	instructionnum++
+    return statement("Goodbye")
 
 @ask.intent("AMAZON.RepeatIntent")
 def repeat_intent():
-    return statement("repeat")
+	instructionnum++
+	return statement(step1)
 
 @ask.intent("AMAZON.NextIntent")
 def next_intent():
-    return statement("Next!")
-
+	instructionnum++
+	if instructionnum == 3:
+		return statement(step2)
+	if instructionnum == 5:
+		return statement(step3)
+	return statement("You have completed the guide. Would you like to start a new project?")
+	
 @ask.intent("AMAZON.PreviousIntent")
 def previous_intent():
-    return statement("previous!")
+	instructionnum++
+    return statement(step2)
 
 @ask.intent("ItemIntent")
 def item_intent():
+	instructionnum++
     return statement("I sent a list of possible guides through the alexa app. Please choose one or try searching again.")
