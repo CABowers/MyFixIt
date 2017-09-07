b1f30e8d4fec5a7c2b5bc3fb91304970d0d92b26
Cole Bowers <cole.a.bowers@gmail.com>
2017-09-06 21:11:43 -0400

    Enable API to work with searching/navigating

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from flask import Flask
 from flask_ask import Ask, statement, question, session
 from pyfixit import *
+import logging
+
+logger = logging.getLogger()
 
 app = Flask(__name__)
 ask = Ask(app, "/")
@@ -27,14 +30,23 @@ def start_skill():
 
 @ask.intent("SearchIntent")
 def search(item):
-    get_guides(item)
+    if item is None:
+        logger.info("Item is None")
+        get_guides("Stapler")
+    else:
+        get_guides(item)
     guide_names = "\n".join(g.title for g in guides)
     return question("Here are your search results. Please select a guide by reading the title.").simple_card(title="Guides",content=guide_names)
 
 
 @ask.intent("SelectGuideIntent")
 def selectguide(title):
-    found = select_guide(title)
+    found = False;
+    if title is None:
+        logger.info("Title was None")
+        found = select_guide("Stapler Maintenance")
+    else:
+        found = select_guide(title)
     if found:
         return question("You have selected guide {} . Say next to begin instructions".format(guide.title))
     return question("Please select a valid guide.")
@@ -86,12 +98,6 @@ def previous_intent():
     return question(text_for_step(steps[instruction_num]))
 
 
- # TODO: new search intent replaces this. When we remove this, remove it from sample utterances and Intent Schema
-@ask.intent("ItemIntent")
-def item_intent():
-    return question("I sent a list of possible guides through the alexa app. Please choose one or try searching again.").simple_card(title="Guides",content="Stapler Maintenance\nSearch Again")
-
-
 def get_guides(search):
     global guides
     guides = Category(search).guides
@@ -112,7 +118,7 @@ def select_guide(title):
     global instruction_num
     found = False
     for g in guides:
-        if g.title == title:
+        if g.title.lower() == title.lower():
             guide = g
             steps = g.steps
             instruction_num = -1
@@ -123,10 +129,9 @@ def select_guide(title):
 def text_for_step(step):
     step_text = ""
     for line in step.lines:
-        text = '(%s) %s' % (line.bullet, line.text)
-        step_text = "{}\n{}".format(step_text, text)
+        step_text = "{}\n{}".format(step_text, line.text)
     return step_text
-    
+
 
 def get_guide_titles():
     titles = [g.title for g in guides]
