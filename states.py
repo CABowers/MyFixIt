d9d4b4856a5268f11fb2a22d14398ac57d941ec6
ksubrmn <ksubramanian33@gatech.edu>
2017-09-13 14:29:02 -0400

    Adds alexafsm states

--- states.py
+++ states.py
@@ -1,3 +1,6 @@
 from alexafsm.states import with_transitions, States as StatesBase
 from alexafsm import response
 from alexafsm import amazon_intent
+
+SKILL_NAME = "MyFixIt"
+DEFAULT_PROMPT = "What would you like to fix today?"
