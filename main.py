c4854ef86b50d79f7fd0242ef72fc913c0479192
cbranson3 <cbranson3@gatech.edu>
2017-09-13 14:51:45 -0400

    Will the first image associated with a step if it exists

--- main.py
+++ main.py
@@ -86,9 +86,11 @@ def next_intent():
         instruction_num -= 1
         return question(done_steps).reprompt("I missed that." + done_steps)
     for image in steps[instruction_num].media:
-        statement("").standardCard(title="Step %i" % instruction_num,
-                                   text="", small_image_url=image.thumbnail,
-                                   large_image_url=image.original)
+        if image.thumbnail and image.original:
+            return question(text_for_step(steps[instruction_num])).standard_card(title="Step %i" % instruction_num,
+                                       text="",
+                                       small_image_url=image.thumbnail,
+                                       large_image_url=image.original)
     return question(text_for_step(steps[instruction_num]))
 
 
