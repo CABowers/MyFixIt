1fccdeb6a92e58f83f6fbcce5fb15b76397b2bf8
cbranson3 <cbranson3@gatech.edu>
2017-10-24 18:28:09 -0400

    Added reprompts

--- main.py
+++ main.py
@@ -87,7 +87,7 @@ def repeat_intent():
         return question(no_steps)
     if instruction_num > len(steps):
         return question(done_steps)
-    return question(text_for_step(steps[instruction_num]))
+    return question(text_for_step(steps[instruction_num])).reprompt("Say next when you are ready to begin the next step.")
 
 
 @ask.intent("AMAZON.NextIntent")
@@ -116,7 +116,7 @@ def next_intent():
             reply = "We have sent an image associated with this step to your Alexa app." \
                     + text_for_step(steps[instruction_num])
         elif len(good_images) == 0:
-            return question(text_for_step(steps[instruction_num]))
+            return question(text_for_step(steps[instruction_num])).reprompt("Can you repeat that?")
 
         if reply:
             return question(reply).simple_card(title="Step %i" % (instruction_num + 1),
@@ -137,10 +137,10 @@ def previous_intent():
         if instruction_num < 0:
             instruction_num += 1
             session.attributes['instruction_num'] = instruction_num
-            return question(no_steps)
+            return question(no_steps).reprompt("I missed that." + no_steps)
         if instruction_num >= len(steps):
             return question(done_steps).reprompt("I missed that." + done_steps)
-        return question(text_for_step(steps[instruction_num]))
+        return question(text_for_step(steps[instruction_num])).reprompt("Say next to proceed to the next step.")
     else:
         return error_exit()
 
@@ -168,7 +168,7 @@ def help_intent():
         response = "I sent a list of guides to your phone, please tell me the number of the guide you would like to complete."
     elif previous == SELECT_GUIDE:
         response == "Please say next if you have selected a valid guide"
-    return question(response)
+    return question(response).reprompt("I don't understand. Can you repeat that?")
 
 #Length of task
 @ask.intent("LengthOfGuideIntent")
@@ -181,13 +181,14 @@ def lenofguide_intent(len_guide_number):
     minutes = (length % (60 * 24)) / 60
     seconds = length % 60
 
-    return question("The length of this guide is %i hours %i minutes and %i seconds" %(hours, minutes, seconds))
+    return question("The length of this guide is %i hours %i minutes and %i seconds" %(hours, minutes, seconds)).reprompt("Say next to continue to the instructions.")
 
 
 #Number of instructions
 @ask.intent("NumberInstructionsIntent")
 def numinstructions_intent():
-    return question("The number of instructions in this guide is %i" %len(steps))
+    return question("The number of instructions in this guide is %i" %len(steps)).reprompt("Say next to continue to the instructions.")
+")
 
 #Current instruction
 @ask.intent("CurrentInstructionIntent")
@@ -195,19 +196,19 @@ def curinstruction_intent():
     num = session.attributes['instruction_num']
     num = num + 1
     if num <= 0:
-        return question("You have not started any instructions yet. Say next to go to the first instruction.")
-    return question("The current instruction number for the current guide is %i" %num)
+        return question("You have not started any instructions yet. Say next to go to the first instruction.").reprompt("Say next to continue to the instructions.")
+    return question("The current instruction number for the current guide is %i" %num).reprompt("Say next to go to the next step.")
 
 #Number of instructions remaining
 @ask.intent("InstructionsLeftIntent")
 def instructionsleft_intent():
     instructions_left = len(steps) - session.attributes['instruction_num']
-    return question("The number of instructions left in this guide is %i" %instructions_left)
+    return question("The number of instructions left in this guide is %i" %instructions_left).reprompt("Say next to go to the next step.")
 
 #Difficulty of the instruction guide
 @ask.intent("DifficultyIntent")
 def difficulty_intent():
-    return question("The difficulty of the guide is " + guide.difficulty)
+    return question("The difficulty of the guide is " + guide.difficulty).reprompt("Say next to continue to the instructions.")
 
 
 @ask.intent("NextPicture")
